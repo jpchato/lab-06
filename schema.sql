f933c936038ae4aad07a2a090e5abcc1fdba52ec
Jesse Pena <jpchato@gmail.com>
2020-03-20 08:36:50 -0700

    added changes from class demo

--- schema.sql
+++ schema.sql
@@ -5,6 +5,6 @@ CREATE TABLE locations
   id SERIAL PRIMARY KEY,
   search_query VARCHAR(255),
   formatted_query VARCHAR(255),
-  latitude VARCHAR(255),
-  longitude VARCHAR(255)
+  latitude float,
+  longitude float
 );
